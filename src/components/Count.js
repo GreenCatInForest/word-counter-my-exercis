bc59468bbca54b377b5589b6a9afe73e672569df
GreenCatInForest <event2016.2017@gmail.com>
2023-06-26 18:27:56 +0100

    completed

--- src/components/Count.js
+++ src/components/Count.js
@@ -2,6 +2,14 @@ import { useState } from "react";
 
 export const Count = () => {
   const [wordCount, setWordCount] = useState(0);
+
+  const handleOnChange = (event) => {
+    let value = event.target.value.trim();
+    const words = value.split(" ");
+    const wordsLength = words.length;
+    setWordCount(wordsLength);
+  };
+
   return (
     <div>
       <div>
@@ -9,9 +17,10 @@ export const Count = () => {
           rows={10}
           cols={30}
           placeholder="Type please your text here"
+          onChange={handleOnChange}
         ></textarea>
       </div>
-      <div>Word count: 0</div>
+      <div>Word count: {wordCount}</div>
     </div>
   );
 };
